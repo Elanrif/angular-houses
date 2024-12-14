8887f7975536bab7903ce4bb1c456e84f4989f17
Elanrif <saidbacoelanrif@gmail.com>
2024-12-15 00:01:02 +0100

    feat: best way to filter with toUpper or toLowerCase method and includes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,13 +10,13 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filtered/>
+        <button class="primary" type="button" (click)="filterResults(filtered.value)">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
       />
     </section>
@@ -25,9 +25,23 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = []
   housingService: HousingService = inject(HousingService);
   
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocation();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => 
+    housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+  );
+
   }
 }
